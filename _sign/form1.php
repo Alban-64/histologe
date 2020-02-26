9629a6bed8ed5edc5e04c7a0f7ad7e437597c5a0
Alban Sestiaa <60881966+Alban-64@users.noreply.github.com>
2020-02-26 15:19:09 +0100

    test

---
+++ _sign/form1.php
@@ -0,0 +1,11 @@
+<form>
+Votre adresse courriel : <input type=text size=20 name=courriel> <br>
+Votre adresse : numéro : <input type=text size=4 name=numrue> Rue <input type=text size=4 name=librue> <br>
+
+</form>
+
+
+<?php
+echo "GO";
+
+
